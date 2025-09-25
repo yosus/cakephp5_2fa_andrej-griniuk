2c1bbad33c0645306bc02fded426cfd69fc09012
yosus <5551430+yosus@users.noreply.github.com>
2025-09-25 22:20:34 +0800

    Update UsersController.php
    add action

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -83,6 +83,26 @@ class UsersController extends AppController
     }
 
 
+    /**
+     * Add method
+     *
+     * @return \Cake\Http\Response|null|void Redirects on successful add, renders view otherwise.
+     */
+    private function add()
+    {
+        $user = $this->Users->newEmptyEntity();
+        if ($this->request->is('post')) {
+            $user = $this->Users->patchEntity($user, $this->request->getData());
+            if ($this->Users->save($user)) {
+                $this->Flash->success(__('The user has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The user could not be saved. Please, try again.'));
+        }
+        $this->set(compact('user'));
+    }
+    
 
     /** Adds 2fa secret to user database
      * @return \Cake\Http\Response|null|void Renders view
